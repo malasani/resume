b3fa22ae547799e3093c0b71e982ba725b191781
v-bamala <tony.darsi3@gmail.com>
2017-05-07 11:17:54 +0530

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 var app = express();
-app.set("port", process.env.port || 5000);
+app.set("port", process.env.PORT  || 5000);
 
 app.use(express.static(__dirname + '/dist'));
 
